d393354a22df67e9c3a5b595ba8da8c1003a4e21
Jimmy McCarthy <jimmy.mccarthy@gmail.com>
2018-05-25 17:23:16 -0600

    Functional query builder; Functional query-scryfall stuff. Filled out the actual queries for all the initial types, and all the colors.

--- mtgstone/mtgstone/consts.py
+++ mtgstone/mtgstone/consts.py
@@ -6,20 +6,95 @@ need to keep static data here, like what formats we support, what queries
 to use for various things on scryfall, 
 """
 
+QUERY_URL = 'https://api.scryfall.com/cards/search'
+
 FORMATS = [
-	"Old School 93-94",
+    "Old School 93-94",
 ]
 
 RESTRICTIONS = {
-	"Old School 93-94": [
-		"Black Lotus",
-		"Mox Sapphire", # TODO: Add more! 
-	],
+    "Old School 93-94": [
+        "Ancestral Recall",
+        "Balance",
+        "Black Lotus",
+        "Braingeyser",
+        "Chaos Orb",
+        "Channel",
+        "Demonic Tutor",
+        "Library of Alexandria",
+        "Mana Drain",
+        "Maze of Ith",
+        "Mind Twist",
+        "Mox Emerald",
+        "Mox Jet",
+        "Mox Pearl",
+        "Mox Ruby",
+        "Mox Sapphire",
+        "Recall",
+        "Regrowth",
+        "Sol Ring",
+        "Time Vault",
+        "Time Walk",
+        "Timetwister",
+        "Wheel of Fortune",
+    ],
 }
 
+BANNINGS = {
+    "Old School 93-94": [
+        "Bronze Tablet",
+        "Contract from Below",
+        "Darkpact",
+        "Demonic Attorney",
+        "Jeweled Bird",
+        "Rebirth",
+        "Tempest Efreet",
+    ]
+}
+
+BASE_QUERIES = {
+    "Old School 93-94": "year<=1994 unique:cards not:reprint",
+}
+
+COLOR_MIXINS = {
+    "white": "c=w",
+    "blue": "c=u",
+    "black": "c=b",
+    "red": "c=r",
+    "green": "c=g",
+    "wu": "(c=w or c=u or c=wu)",
+    "wb": "(c=w or c=b or c=wb)",
+    "wr": "(c=w or c=r or c=wr)",
+    "wg": "(c=w or c=g or c=wg)",
+    "ub": "(c=u or c=b or c=ub)",
+    "ur": "(c=u or c=r or c=ur)",
+    "ug": "(c=u or c=g or c=ug)",
+    "br": "(c=b or c=r or c=br)",
+    "bg": "(c=b or c=g or c=bg)",
+    "rg": "(c=r or c=g or c=rg)",
+}
+
+# This is all the magic queries we make to Scryfall to get lists of cards.
+# The query is pretty obvious; colors gets filled in right before we execute.
+# The weight parameter lets us make it so certain categories come up more (e.g. more creatures)
+# For now, all the weights are 1; tuning this will probably suck a lot, but whatever.
 QUERIES = {
-	"Old School 93-94": {
-		"Creatures": "", # TODO: Add more
-	}
-}
-
+    "Old School 93-94": {
+        "1 or 2 Cost Creatures": {"query":"t:creature (cmc=1 or cmc=2) (%(colors)s or c=c)", "weight": 1},
+        "3 Cost Creatures": {"query":"t:creature cmc=3 (%(colors)s or c=c)", "weight": 1},
+        "4 Cost Creatures": {"query":"t:creature cmc=4 (%(colors)s or c=c)", "weight": 1},
+        "5 Cost Creatures": {"query":"t:creature cmc=5 (%(colors)s or c=c)", "weight": 1},
+        "6+ Cost Creatures": {"query":"t:creature cmc>=6 (%(colors)s or c=c)", "weight": 1},
+        "Auras": {"query":"t:aura (%(colors)s or c=c)", "weight": 1},
+        "Enchantments": {"query":"t:enchantment (%(colors)s or c=c)", "weight": 1},
+        "Artifacts": {"query":"t:artifact", "weight": 1},
+        "Sorceries": {"query":"t:sorcery (%(colors)s or c=c)", "weight": 1},
+        "Instants": {"query":"t:instant (%(colors)s or c=c)", "weight": 1},
+        "Nonbasic Lands": {"query":"t:land -t:basic", "weight": 1},
+        # TODO: Clean this up to only provide mana of the correct colors (from lands); all colors ok from spells.
+        "Mana Acceleration": {"query":"(t:land -t:basic o:/add {(W|U|B|R|G|C)}{(W|U|B|R|G|C)}/) or (-t:land o:/add {(W|U|B|R|G|C)}/ (%(colors)s or c=c))", "weight": 1},
+        "Legends": {"query":"t:legend (%(colors)s or c=c)", "weight": 1},
+        # "2 Cost Creatures": "t:creature cmc=2 (%(colors)s or c=c)",
+        # "Removal": "", # Not sure how to query this one yet.
+    }
+} 
--- mtgstone/mtgstone/tools.py
+++ mtgstone/mtgstone/tools.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, print_function
+import consts
+import requests
 
 """
 This is where I store various conversion tools, as there's a lot of conversions
@@ -7,23 +9,61 @@ of current user data
 """
 
 def encoded_deck_info_to_deck_info(encoded_blob):
-	"""
-	Given a b64 encoded deck_info blob, return a string of if
-	"""
-	# TODO: Actually do this
-	try:
-		return "{}"
-	except:
-		return "{}"
+    """
+    Given a b64 encoded deck_info blob, return a string of it
+    """
+    # TODO: Actually do this
+    try:
+        return "{}"
+    except:
+        return "{}"
 
 
 def deck_info_to_dict(deck_info):
-	"""
-	Given a deck_info blob (a string)
-	"""
-	return {}
+    """
+    Given a deck_info blob (a string), decode it into a dictionary
+    """
+    return {}
 
 def encoded_to_dict(encoded_blob):
-	"""Get and encoded blob, return a nice python dict of deck_info"""
-	# TODO: This probably needs some error checking?
-	return deck_info_to_dict(ncoded_deck_info_to_deck_info(encoded_blob))
+    """Get an encoded blob, return a nice python dict of deck_info"""
+    # TODO: This probably needs some error checking?
+    return deck_info_to_dict(ncoded_deck_info_to_deck_info(encoded_blob))
+
+def scryfall_query_builder(deck_colors, query_type, deck_format):
+    """
+    Build the query argument that we're gonna send to Scryfall to get our list of cards.
+    """
+    base_query = consts.BASE_QUERIES[deck_format]
+    color_mixin = consts.COLOR_MIXINS[deck_colors]
+    query_params = consts.QUERIES[deck_format][query_type]['query'] % {'colors': color_mixin}
+
+    full_query = ' '.join([base_query, query_params])
+
+    return full_query
+
+def get_cards(query_param):
+    """
+    Someday if I add caching of queries, this is the method that will populate/check the cache.
+    For now, it just passes through the query.
+
+    If anyone actually uses this, adding caching should be really simple; just need a place to store the data.
+    Scryfall doesnt really want unauthed accounts blasting their API (for good reason).
+    """
+    return query_scryfall(query_param)
+
+def query_scryfall(query_param):
+    """
+    Reach out to scryfall and get our card data.
+    """
+    params = 'q=%s' % query_param
+    result = requests.get(consts.QUERY_URL, params)
+    try:
+        result.raise_for_status()
+    except Exception as e:
+        print('Bad stuff: ')
+        print(result.content)
+        raise
+
+    return result.json()
+
