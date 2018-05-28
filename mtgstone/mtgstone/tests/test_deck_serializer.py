8500b22ba7e71427fc8e8433151626e987bfb703
Jimmy McCarthy <jimmy.mccarthy@gmail.com>
2018-05-27 18:59:29 -0600

    Starting to organize the tests I want to write for the various components

---
+++ mtgstone/mtgstone/tests/test_deck_serializer.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+"""
+Test out all the deck serialization junk
+"""
+import pytest
+import mtgstone.tools as tools
+import mtgstone.consts as consts
+
+class TestDecklistToEncodedString(object):
+    """
+    These tests deal with taking a nice Python decklist object and turning it into a b64 encoded string,
+    and everything along the way
+    """
+
+    def test_success(self):
+        pass
+
+    def test_(self):
+        pass
+
+    def test_(self):
+        pass
+
+    def test_(self):
+        pass
+
+    def test_(self):
+        pass
+
+    def test_(self):
+        pass
